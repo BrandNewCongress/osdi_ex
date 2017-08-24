200cec59e5418b8163adaa8a42fec1d9a131a37d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-08-24 16:03:31 -0400

    better test structure

--- test/osdi_ex_test.exs
+++ test/osdi_ex_test.exs
@@ -1,38 +1,3 @@
 defmodule OsdiTest do
   use ExUnit.Case
-
-  test "standard email signup" do
-    person = %Osdi.Person{}
-
-    [first_name, last_name, email_chunk] =
-      :alphanumeric
-      |> StreamData.string()
-      |> Stream.filter(fn str -> String.length(str) > 5 end)
-      |> Enum.take(3)
-
-    signup = Osdi.Person.signup_email(person,
-      %{given_name: first_name, family_name: last_name,
-        email: email_chunk <> "@comcast.edu"})
-
-    assert {:ok, _person} = Osdi.Repo.insert(signup)
-  end
-
-  test "second email signup" do
-    person = Osdi.Repo.all(Osdi.Person) |> List.first()
-
-    %{given_name: first_name, family_name: last_name} = person
-
-    [email_chunk] =
-      :alphanumeric
-      |> StreamData.string()
-      |> Stream.filter(fn str -> String.length(str) > 5 end)
-      |> Enum.take(1)
-
-    signup = Osdi.Person.signup_email(person,
-      %{given_name: first_name, family_name: last_name,
-        email: email_chunk <> "@comcast.edu"})
-
-    assert {:ok, %Osdi.Person{email_addresses: more_than_one}} = Osdi.Repo.update(signup)
-    assert more_than_one |> length() > 1
-  end
 end
