827536212d5b55f2c7667ddf28b70d1131a26e66
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-06 14:59:57 -0400

    forgot embedded host file

---
+++ lib/osdi/embedded/host.ex
@@ -0,0 +1,16 @@
+defmodule Osdi.Host do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :name, :string
+    field :phone_number, :string
+    field :email_address, :string
+    field :public, :boolean, default: true
+  end
+
+  def changeset(host, params \\ %{}) do
+    host
+    |> cast(params, [:name, :phone_number, :email_address, :public])
+  end
+end
