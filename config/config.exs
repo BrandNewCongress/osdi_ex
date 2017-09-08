f8388376d064aa23f79e3144fd045e8ad7e47f66
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-08 16:26:29 -0400

    optional secret include for migrations

--- config/config.exs
+++ config/config.exs
@@ -37,3 +37,5 @@ config :osdi, ecto_repos: [Osdi.Repo]
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
+
+# import_config "secret.exs"
