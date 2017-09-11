6f59b0f3a52ef1d1931fa08195274c3b2cdc1059
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-11 11:35:36 -0400

    attendance.push

--- lib/osdi/attendance.ex
+++ lib/osdi/attendance.ex
@@ -1,6 +1,8 @@
 defmodule Osdi.Attendance do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+  alias Osdi.{Repo, EmailAddress, Address, PhoneNumber, Event}
 
   @base_attrs ~w(origin_system action_date status attended)a
   @associations ~w(person event)a
@@ -26,5 +28,35 @@ defmodule Osdi.Attendance do
     |> cast_embed(:referrer_data)
     |> put_assoc(:person, params.person)
     |> put_assoc(:event, params.event)
+  end
+
+  def push(event_id, person) do
+    %{given_name: first_name, family_name: last_name, email_address: email_address,
+      phone_number: phone_number, postal_address: postal_address} = person
+
+    %{id: person_id} = person = Osdi.Person.push(%{given_name: first_name, family_name: last_name,
+      email_addresses: [%{address: email_address, primary: true}] |> Enum.map(&EmailAddress.get_or_insert/1),
+      phone_numbers: [%{number: phone_number, primary: true}] |> Enum.map(&PhoneNumber.get_or_insert/1),
+      postal_addresses: [postal_address] |> Enum.map(&Address.get_or_insert/1),
+    })
+
+    existing =
+      (from a in Osdi.Attendance, where: a.event_id == ^event_id and a.person_id == ^person_id)
+      |> Repo.all()
+      |> List.first()
+
+    case existing do
+      nil ->
+        event = Repo.get(Event, event_id)
+
+        attendance = %Osdi.Attendance{
+          action_date: DateTime.utc_now(), status: "accepted",
+          person: person, event: event, referrer_data: %{}
+        }
+
+        Repo.insert!(attendance)
+
+      attendance -> attendance
+    end
   end
 end
--- test/model_tests/attendance_test.exs
+++ test/model_tests/attendance_test.exs
@@ -2,7 +2,7 @@ defmodule AttendanceTest do
   use ExUnit.Case
   import ShorterMaps
 
-  alias Osdi.{Repo, Event, Person, Attendance}
+  alias Osdi.{Repo, Event, Person, Attendance, Address}
 
   test "new rsvp" do
     event = Repo.all(Event) |> Enum.take(1) |> List.first()
@@ -20,5 +20,28 @@ defmodule AttendanceTest do
     ))
 
     {:ok, _rsvp} = Repo.insert(new_rsvp)
+  end
+
+  test "attendance push" do
+    event = Repo.all(Event) |> Enum.take(1) |> List.first()
+
+    rsvp_data =
+      %{given_name: Faker.Name.first_name(), family_name: Faker.Name.last_name(),
+        email_address: Faker.Internet.email(), phone_number: Faker.Phone.EnUs.phone(),
+        postal_address: %Address{
+          venue: "The Office",
+          address_lines: ["714 S Gay St."],
+          locality: "Knoxville",
+          region: "TN",
+          postal_code: "37902",
+          time_zone: "America/New_York",
+          location: %Geo.Point{coordinates: {35.9630028, -83.918997}, srid: nil}
+      }}
+
+    %{id: first_attendance_id, person_id: first_person_id} = Attendance.push(event.id, rsvp_data)
+    %{id: second_attendance_id, person_id: second_person_id} = Attendance.push(event.id, rsvp_data)
+
+    assert first_person_id = second_person_id
+    assert first_attendance_id = second_attendance_id
   end
 end
