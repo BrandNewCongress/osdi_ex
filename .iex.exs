d6163ee39e9545241c9d2aa33e6ec0d79dfb6c9a
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-10-31 14:32:10 -0700

    address get or insert is perfect, and iex defaults"

---
+++ .iex.exs
@@ -0,0 +1,5 @@
+import Ecto
+import Ecto.Query
+import Ecto.Changeset
+
+alias Osdi.{Repo, Person, Event, EmailAddress, Donation, PhoneNumber, Address, Contact, Tag, Tagging}
--- lib/models/address.ex
+++ lib/models/address.ex
@@ -46,29 +46,49 @@ defmodule Osdi.Address do
     |> cast(params, @base_attrs)
   end
 
-  def get_or_insert(
-        address = %Osdi.Address{address_lines: address_lines, locality: locality, region: region}
-      )
-      when is_list(address_lines) and not is_nil(region) do
+  def get_or_insert(address = %Osdi.Address{address_lines: address_lines})
+      when is_list(address_lines) do
     address_line_zero =
       case address_lines do
         nil -> nil
         list -> list |> List.first()
       end
 
-    matches =
-      Osdi.Repo.all(
-        from(
-          a in Osdi.Address,
-          where: ^address_line_zero in a.address_lines and a.locality == ^locality and
-            a.region == ^region
-        )
-      )
+    query = dynamic([a], ^address_line_zero in a.address_lines)
+
+    no_conflicts =
+      Map.take(address, ~w(locality region postal_code)a)
+      |> Enum.filter(fn {_key, val} -> val != nil end)
+
+    query =
+      case address.locality do
+        nil -> query
+        locality -> dynamic([a], (is_nil(a.locality) or a.locality == ^locality) and ^query)
+      end
+
+    query =
+      case address.postal_code do
+        nil ->
+          query
+
+        postal_code ->
+          dynamic([a], (is_nil(a.postal_code) or a.postal_code == ^postal_code) and ^query)
+      end
+
+    query =
+      case address.region do
+        nil -> query
+        region -> dynamic([a], (is_nil(a.region) or a.region == ^region) and ^query)
+      end
+
+    matches = Osdi.Repo.all(from(a in Osdi.Address, where: ^query))
 
     existing =
       matches
       |> Enum.take(1)
       |> List.first()
+
+    IO.inspect(existing)
 
     case existing do
       nil -> Osdi.Repo.insert!(address)
@@ -76,8 +96,14 @@ defmodule Osdi.Address do
     end
   end
 
-  def get_or_insert(address = %Osdi.Address{}), do: address
-  def get_or_insert(address = %{}), do: address
+  def get_or_insert(address = %Osdi.Address{}) do
+    address
+  end
+
+  def get_or_insert(address = %{}),
+    do: Osdi.Address
+        |> struct(address)
+        |> get_or_insert()
 
   def update_coordinates(address = %Osdi.Address{id: _id}, {latitude, longitude}) do
     new_geo_point = %Geo.Point{coordinates: {latitude, longitude}, srid: nil}
